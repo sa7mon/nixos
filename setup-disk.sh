76dddf1964d2d35090965fd0d02d92e9ad4811bc
Dan Salmon <sa7mon@users.noreply.github.com>
2021-11-01 18:14:01 -0500

    Create setup-disk.sh

---
+++ setup-disk.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+parted /dev/sda -- mklabel msdos
+parted /dev/sda -- mkpart primary 1MiB -8GiB
+parted /dev/sda -- mkpart primary linux-swap -8GiB 100%
+
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+
+mount /dev/disk/by-label/nixos /mnt
+swapon /dev/sda2
+
+nixos-generate-config --root /mnt
